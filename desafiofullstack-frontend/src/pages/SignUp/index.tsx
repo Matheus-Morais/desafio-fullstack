56c13a78feb11ba95fa32acea90977f468231fe0
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-09-16 17:24:33 -0300

    fix: adição dos inputs

--- desafiofullstack-frontend/src/pages/SignUp/index.tsx
+++ desafiofullstack-frontend/src/pages/SignUp/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Input from '../../components/Input';
+
 import {
     Container,
     Content,
     Logo,
+    Info,
     Form,
+    ContainerUseTerms,
+    Link
 } from './styles';
 
 const SignUp: React.FC = () => {
@@ -13,7 +18,26 @@ const SignUp: React.FC = () => {
             <Content>
                 <Logo />
                 <Form>
-                    <p>Informe seus dados</p>
+                    <Info fontWeight={'600'} fontSize={'16'} marginBottom={'30'} marginTop={'44'}>Informe seus dados</Info>
+
+                    <Input name="name" iconName="UserAltSolid" placeholder="Nome" />
+
+                    <Input name="last_name" iconName="UserCircleSolid" placeholder="Sobrenome" />
+
+                    <Input name="email" iconName="EnvelopeSolid" placeholder="Email Pessoal" />
+
+                    <Input name="phone" iconName="Brazil" placeholder="" />
+
+                    <Input name="password" iconName="UnlockSolid" placeholder="Senha" />
+
+                    <Input name="confirm_password" iconName="UnlockSolid" placeholder="Confirmar senha" />
+
+                    <ContainerUseTerms>
+                        <Info fontWeight={'normal'} fontSize={'12'} marginBottom={'18'} marginTop={'8'}>
+                            Ao se cadastrar você automaticamente concorda com nossos
+                        <Link href="#"> Termos de Uso</Link>
+                        </Info>
+                    </ContainerUseTerms>
                 </Form>
             </Content>
         </Container>
