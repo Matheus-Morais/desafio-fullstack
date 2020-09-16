b721921427b4c0de3ed962f5fdd0a84340198050
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-09-16 10:49:41 -0300

    fix: normalizando o nome dos campos da tabela em ingles

--- desafiofullstack-backend/src/config/database/migrations/20200916103157_create_user.js
+++ desafiofullstack-backend/src/config/database/migrations/20200916103157_create_user.js
@@ -1,16 +1,16 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable('usuarios', function (table) {
+    return knex.schema.createTable('users', function (table) {
         table.increments()
-        table.string('nome').notNullable()
-        table.string('sobrenome').notNullable()
+        table.string('name').notNullable()
+        table.string('last_name').notNullable()
         //O numero de celular no brasil tem por padrão 13 digitos
-        table.string('numero', 13).notNullable().unique()
+        table.string('phone', 13).notNullable().unique()
         table.string('email').notNullable().unique()
-        table.string('senha').notNullable()
+        table.string('password').notNullable()
     })
 };
 
 exports.down = function (knex) {
-    knex.schema.dropTable('usuarios')
+    knex.schema.dropTable('users')
 };
