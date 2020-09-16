5223a1819a6502197cdad76e2d26d8e9d67c77ec
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-09-16 15:49:29 -0300

    feat: criação da pagina de cadastro inicial

---
+++ desafiofullstack-frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+    return (
+        <div>
+            <p>Hello SignUp</p>
+        </div>
+    );
+}
+
+export default SignUp;
